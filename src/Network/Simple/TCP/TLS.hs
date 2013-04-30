dcf78e5e3e0ee3f446e95f7d177178a10bac12b4
Renzo Carbonara <gnuk0001@gmail.com>
2013-04-30 02:12:09 -0300

    Documentation and cleanup!!

--- src/Network/Simple/TCP/TLS.hs
+++ src/Network/Simple/TCP/TLS.hs
@@ -1,14 +1,15 @@
--- | This mdule exports functions that abstract simple usage patterns
--- for establishing TLS-secured TCP connections, relevant to both the
--- client side and server side of the connection.
+-- | This module exports common usage patterns for establishing TLS-secured
+-- TCP connections, relevant to both the client side and server side of the
+-- connection.
 --
--- Consider using the @network-simple@ package if you would like to use
--- a similar API without TLS support.
+-- This module re-exports some functions from the "Network.Simple.TCP" module
+-- in the @network-simple@ package. Consider using that module directly if you
+-- need a similar API without TLS support.
 
 module Network.Simple.TCP.TLS (
   -- * Server side
     serve
-  , serveFork
+  , serveParams
   -- ** Listening
   , S.listen
   -- ** Accepting
@@ -25,6 +26,10 @@ module Network.Simple.TCP.TLS (
   , S.bindSock
   , connectTls
   , acceptTls
+
+  -- * Default values
+  , setDefaultServerParams
+  , setDefaultClientParams
   ) where
 
 import           Control.Concurrent         (ThreadId, forkIO)
@@ -42,132 +47,163 @@ import           System.Certificate.X509    (getSystemCertificateStore)
 
 --------------------------------------------------------------------------------
 
--- | Start a TCP server that sequentially accepts and uses each incoming
--- connection.
+-- | Start a TLS-secured TCP server that accepts incoming connections and
+-- handles each of them concurrently, in different threads.
 --
--- Both the listening and connection sockets are closed when done or in case of
--- exceptions.
---
--- Note: You don't need to use 'listen' nor 'accept' manually if you use this
--- function.
-serve
-  :: (X509, T.PrivateKey)
-  -> S.HostPreference   -- ^Preferred host to bind.
-  -> NS.ServiceName   -- ^Service port to bind.
-  -> ((T.Context, NS.SockAddr) -> IO r)
-                      -- ^Computation to run once an incoming
-                      -- connection is accepted. Takes the connection socket
-                      -- and remote end address.
-  -> IO r
-serve cpk hp port k = do
-    S.listen hp port $ \(lsock,_) -> do
-      forever $ accept cpk lsock k
-
--- | Start a TCP server that accepts incoming connections and uses them
--- concurrently in different threads.
+-- The TLS connection is configured with the given 'X509' certificate and
+-- 'T.PrivateKey' using some default settings (see 'setDefaultServerParams').
+-- Use 'serveParams' if you need more control on how the TLS connection is
+-- configured.
 --
 -- The listening and connection sockets are closed when done or in case of
 -- exceptions.
 --
--- Note: You don't need to use 'listen' nor 'acceptFork' manually if you use
--- this function.
-serveFork
-  :: (X509, T.PrivateKey)
-  -> S.HostPreference   -- ^Preferred host to bind.
-  -> NS.ServiceName   -- ^Service port to bind.
+-- Note: If you use this function then you don't need to manually use 'listen'
+-- nor 'acceptFork', nor manually close the obtained 'T.Context', nor perform a
+-- TLS 'T.handshake'. All that is already handled for you.
+serve
+  :: (X509, T.PrivateKey) -- ^Server certificate and private key.
+  -> S.HostPreference     -- ^Preferred host to bind.
+  -> NS.ServiceName       -- ^Service port to bind.
   -> ((T.Context, NS.SockAddr) -> IO ())
-                      -- ^Computation to run in a different thread
-                      -- once an incoming connection is accepted. Takes the
-                      -- connection socket and remote end address.
+                          -- ^Computation to run in a different thread
+                          -- once an incomming connection is accepted and a
+                          -- TLS-secured communication is established. Takes the
+                          -- TLS connection context and remote end address.
   -> IO ()
-serveFork cpk hp port k = do
+serve cpk = serveParams $ setDefaultServerParams cpk T.defaultParamsServer
+
+
+-- | Like 'serve', except you can give explicit TLS configuration 'T.Params'.
+serveParams
+  :: T.Params             -- ^TLS connection configuration parameters.
+  -> S.HostPreference     -- ^Preferred host to bind.
+  -> NS.ServiceName       -- ^Service port to bind.
+  -> ((T.Context, NS.SockAddr) -> IO ())
+                          -- ^Computation to run in a different thread
+                          -- once an incomming connection is accepted and a
+                          -- TLS-secured communication is established. Takes the
+                          -- TLS connection context and remote end address.
+  -> IO ()
+serveParams params hp port k = do
     S.listen hp port $ \(lsock,_) -> do
-      forever $ acceptFork cpk lsock k
+      forever $ acceptForkParams params lsock k
 
 --------------------------------------------------------------------------------
 
--- | Accept a single incoming connection and use it.
+-- | Accept a single incomming TLS-secured TCP connection, perform a TLS
+-- handshake and use the connection.
 --
--- The connection socket is closed when done or in case of exceptions.
+-- The TLS connection is configured with the given 'X509' certificate and
+-- 'T.PrivateKey' using some default settings (see 'setDefaultServerParams').
+-- Use 'acceptParams' if you need more control on how the TLS connection is
+-- configured.
+--
+-- The connection socket is closed when done or in case of exceptions. If you
+-- need to manage the lifetime of the connection resources yourself, then use
+-- 'acceptTls' instead.
 accept
-  :: (X509, T.PrivateKey)
-  -> NS.Socket        -- ^Listening and bound socket.
+  :: (X509, T.PrivateKey) -- ^Server certificate and private key.
+  -> NS.Socket            -- ^Listening and bound socket.
   -> ((T.Context, NS.SockAddr) -> IO b)
-                      -- ^Computation to run once an incoming
-                      -- connection is accepted. Takes the connection socket
-                      -- and remote end address.
+                          -- ^Computation to run in a different thread
+                          -- once an incomming connection is accepted and a
+                          -- TLS-secured communication is established. Takes the
+                          -- TLS connection context and remote end address.
   -> IO b
-accept cpk = acceptParams params where
-    params = defModServerParams cpk T.defaultParamsServer
+accept cpk = acceptParams (setDefaultServerParams cpk T.defaultParamsServer)
 {-# INLINABLE accept #-}
 
-
+-- | Like 'accept', except you can give explicit TLS configuration 'T.Params'.
 acceptParams
-  :: T.Params
-  -> NS.Socket        -- ^Listening and bound socket.
+  :: T.Params             -- ^TLS connection configuration parameters.
+  -> NS.Socket            -- ^Listening and bound socket.
   -> ((T.Context, NS.SockAddr) -> IO b)
-                      -- ^Computation to run once an incoming
-                      -- connection is accepted. Takes the connection socket
-                      -- and remote end address.
+                          -- ^Computation to run in a different thread
+                          -- once an incomming connection is accepted and a
+                          -- TLS-secured communication is established. Takes the
+                          -- TLS connection context and remote end address.
   -> IO b
 acceptParams params lsock k = do
     conn@(ctx,_) <- acceptTls params lsock
     E.finally (T.handshake ctx >> k conn)
-              (T.backendClose $ T.ctxConnection ctx)
+              (T.backendClose (T.ctxConnection ctx))
 {-# INLINABLE acceptParams #-}
 
 
+-- | Like 'accept', except it performs the TLS hanshake and runs the given
+-- computation in a different thread.
 acceptFork
-  :: (X509, T.PrivateKey)
-  -> NS.Socket        -- ^Listening and bound socket.
+  :: (X509, T.PrivateKey) -- ^Server certificate and private key.
+  -> NS.Socket            -- ^Listening and bound socket.
   -> ((T.Context, NS.SockAddr) -> IO ())
-                      -- ^Computation to run once an incoming
-                      -- connection is accepted. Takes the connection socket
-                      -- and remote end address.
+                          -- ^Computation to run in a different thread
+                          -- once an incomming connection is accepted and a
+                          -- TLS-secured communication is established. Takes the
+                          -- TLS connection context and remote end address.
   -> IO ThreadId
-acceptFork cpk = acceptForkParams params where
-    params = defModServerParams cpk T.defaultParamsServer
+acceptFork cpk =
+    acceptForkParams (setDefaultServerParams cpk T.defaultParamsServer)
 {-# INLINABLE acceptFork #-}
 
 
+-- | Like 'acceptFork', except you can give explicit TLS configuration
+-- 'T.Params'.
 acceptForkParams
-  :: T.Params
-  -> NS.Socket        -- ^Listening and bound socket.
+  :: T.Params             -- ^TLS connection configuration parameters.
+  -> NS.Socket            -- ^Listening and bound socket.
   -> ((T.Context, NS.SockAddr) -> IO ())
-                      -- ^Computation to run once an incoming
-                      -- connection is accepted. Takes the connection socket
-                      -- and remote end address.
+                          -- ^Computation to run in a different thread
+                          -- once an incomming connection is accepted and a
+                          -- TLS-secured communication is established. Takes the
+                          -- TLS connection context and remote end address.
   -> IO ThreadId
 acceptForkParams params lsock k = do
     conn@(ctx,_) <- acceptTls params lsock
     forkIO $ E.finally (T.handshake ctx >> k conn)
-                       (T.backendClose $ T.ctxConnection ctx)
+                       (T.backendClose (T.ctxConnection ctx))
 {-# INLINABLE acceptForkParams #-}
 
 --------------------------------------------------------------------------------
 
+-- | Connect to a TLS-secured TCP server and use the connection
+--
+-- A TLS handshake is performed immediately after establishing the TCP
+-- connection.
+--
+-- By default the TLS connection is configured to use any of the given 'X509'
+-- certificate and 'T.PrivateKey's, or any the ones made available by the
+-- operating system. See 'setDefaultClientParams' for details. Use 'serveParams'
+-- if you need more control on how the TLS connection is configured.
+--
+-- The connection socket is closed when done or in case of exceptions. If you
+-- need to manage the lifetime of the connection resources yourself, then use
+-- 'connectTls' instead.
 connect
-  :: [(X509, Maybe T.PrivateKey)] -- ^Client certificates
+  :: [(X509, Maybe T.PrivateKey)] -- ^Extra certificates and private keys.
   -> NS.HostName                  -- ^Server hostname.
   -> NS.ServiceName               -- ^Server service port.
   -> ((T.Context, NS.SockAddr) -> IO r)
-                      -- ^Computation taking the TLS communication context
-                      -- and the server address.
+                          -- ^Computation to run after establishing TLS-secured
+                          -- TCP connection to the remote server. Takes the TLS
+                          -- connection context and remote end address.
   -> IO r
 connect certs host port f = do
     cstore <- getSystemCertificateStore
     let check = defCheckCerts cstore host
-        params = defModClientParams certs check host T.defaultParamsClient
+        params = setDefaultClientParams certs check host T.defaultParamsClient
     connectParams params host port f
 
 
+-- | Like 'connect', except you can give explicit TLS configuration 'T.Params'.
 connectParams
-  :: T.Params
-  -> NS.HostName      -- ^Server hostname.
-  -> NS.ServiceName   -- ^Server service port.
+  :: T.Params             -- ^TLS connection configuration parameters.
+  -> NS.HostName          -- ^Server hostname.
+  -> NS.ServiceName       -- ^Server service port.
   -> ((T.Context, NS.SockAddr) -> IO r)
-                      -- ^Computation taking the TLS communication context
-                      -- and the server address.
+                          -- ^Computation to run after establishing TLS-secured
+                          -- TCP connection to the remote server. Takes the TLS
+                          -- connection context and remote end address.
   -> IO r
 connectParams params host port f = E.bracket acq rel use where
     acq = connectTls params host port
@@ -176,10 +212,11 @@ connectParams params host port f = E.bracket acq rel use where
 
 --------------------------------------------------------------------------------
 
--- | Similar to 'S.connectSock', except it returns a secure TLS 'T.Context'
--- instead of a 'NS.Socket'.
+-- | Like 'S.connectSock', except it returns a secure TLS 'T.Context' setup
+-- using the given 'T.Params', instead of a 'NS.Socket'.
 --
--- You need to call 'T.handshake' on the resulting 'T.Context' afterwards.
+-- You need to call 'T.handshake' on the resulting 'T.Context' before using it
+-- for communication purposes.
 connectTls :: T.Params -> NS.HostName -> NS.ServiceName
            -> IO (T.Context, NS.SockAddr)
 connectTls params host port = do
@@ -189,10 +226,11 @@ connectTls params host port = do
     return (ctx, caddr)
 
 
--- | Similar to 'NS.accept', except it returns a secure TLS 'T.Context'
--- instead of a 'NS.Socket'.
+-- | Like to 'NS.accept', except it returns a secure TLS 'T.Context' setup
+-- using the given 'T.Params', instead of a 'NS.Socket'.
 --
--- You need to call 'T.handshake' on the resulting 'T.Context' afterwards.
+-- You need to call 'T.handshake' on the resulting 'T.Context' before using it
+-- for communication purposes.
 acceptTls :: T.Params -> NS.Socket -> IO (T.Context, NS.SockAddr)
 acceptTls params lsock = do
     (csock, caddr) <- NS.accept lsock
@@ -212,22 +250,37 @@ defCheckCerts certStore host = TE.certificateChecks
     , return . TE.certificateVerifyDomain host
     ]
 
--- | Default 'T.Params' updater for the client side of a TLS connection.
-defModClientParams :: [(X509, Maybe T.PrivateKey)]
-                   -> ([X509] -> IO T.CertificateUsage)
-                   -> NS.HostName -> T.Params -> T.Params
-defModClientParams certs onCerts host p =
+-- | Default 'T.Params' setter for the client side of a TLS connection.
+--
+-- * Versions: 'T.TLS10', 'T.TLS11', 'T.TLS12'
+--
+-- * Cyphers: 'TE.ciphersuite_all'
+setDefaultClientParams
+  :: [(X509, Maybe T.PrivateKey)]      -- ^Client certificates and private keys.
+  -> ([X509] -> IO T.CertificateUsage) -- ^Verifies server certificates chain.
+  -> NS.HostName                       -- ^Server hostname.
+  -> T.Params -> T.Params
+setDefaultClientParams certs onCerts host p =
     let modCParams cp = cp
           { T.onCertificateRequest = const (return certs)
           , T.clientUseServerName  = Just host }
     in T.updateClientParams modCParams $ p
-          { T.onCertificatesRecv = onCerts
+          { T.pAllowedVersions   = [T.TLS10, T.TLS11, T.TLS12]
+          , T.onCertificatesRecv = onCerts
           , T.pCertificates      = certs
           , T.pCiphers           = TE.ciphersuite_all }
 
--- | Default 'T.Params' updater for the server side of a TLS connection.
-defModServerParams :: (X509, T.PrivateKey) -> T.Params -> T.Params
-defModServerParams (cert, pk) p =
+-- | Default 'T.Params' setter for the server side of a TLS connection.
+--
+-- * Versions: 'T.TLS11', 'T.TLS12'
+--
+-- * Cyphers: 'TE.ciphersuite_medium'
+--
+-- * Do not request a certificate from client.
+setDefaultServerParams
+  :: (X509, T.PrivateKey) -- ^Server certificate and private key.
+  -> T.Params -> T.Params
+setDefaultServerParams (cert, pk) p =
     let modSParams sp = sp
           { T.serverWantClientCert = False }
     in T.updateServerParams modSParams $ p

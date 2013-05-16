1fdd2bfc98bc33e4593922fd5670aa15cdfcbbb8
Renzo Carbonara <gnuk0001@gmail.com>
2013-05-15 22:10:56 -0300

    Removed unused import

--- examples/https-client.hs
+++ examples/https-client.hs
@@ -3,7 +3,6 @@
 module Main (main) where
 
 import           Control.Applicative        ((<$>), (<*>), pure)
-import qualified Control.Exception          as E
 import qualified Data.ByteString.Char8      as B
 import qualified Data.ByteString.Lazy.Char8 as BL ()
 import           Data.Certificate.X509      (X509)

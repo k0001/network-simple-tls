97a07544c2a504ff9f0d38b4b4ddf26286949b71
Renzo Carbonara <gnuk0001@gmail.com>
2019-11-17 22:29:30 +0200

    nix

--- hs-overlay.nix
+++ hs-overlay.nix
@@ -2,10 +2,11 @@
 self: super: {
   tls = super.callHackage "tls" "1.5.1" {};
   network-simple-tls = super.callPackage ./pkg.nix {};
-#   network-simple =
-#     let src = builtins.fetchGit {
-#           url = "https://github.com/k0001/network-simple";
-#           rev = "e7b293dcc4821880f7cb86a460facdc92c03ac38";
-#         };
-#     in super.callPackage "${src}/pkg.nix" {};
+  network-simple = super.callHackage "network-simple" "0.4.5" {};
+  # network-simple =
+  #   let src = builtins.fetchGit {
+  #         url = "https://github.com/k0001/network-simple";
+  #         rev = "678483e7c451b545dabe1a0354c476bdd8ba8de6";
+  #       };
+  #   in super.callPackage "${src}/pkg.nix" {};
 }
